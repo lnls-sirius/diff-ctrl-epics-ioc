fb45c359bfd341e7f50df110c28c9ff805cbef9b
andrei_pereira <andrei.pereira@lnls.br>
2018-04-27 09:38:38 -0300

    iocBoot/iocDiffCtrl/{parseCMDOpts.sh, run*}: Add option for procServ telnet port

--- iocBoot/iocDiffCtrl/parseCMDOpts.sh
+++ iocBoot/iocDiffCtrl/parseCMDOpts.sh
@@ -1,24 +1,44 @@
 #!/bin/sh
 
-while [ "$#" -gt 0 ]; do
-    case "$1" in
-        "-P") P="$2" ;;
-        "-R") R="$2" ;;
-        "-l"|"--left-ctrl-prefix") CTRL_LEFT="$2" ;;
-        "-r"|"--right-ctrl-prefix") CTRL_RIGHT="$2" ;;
-        "-g"|"--egu") EGU="$2" ;;
-        *) echo "Usage:" >&2
-            echo "  $0 -l CTRL_LEFT -r CTRL_RIGHT [-P P_VAL] [-R R_VAL]" >&2
-            echo >&2
-            echo " Options:" >&2
-            echo "  -l or --left-ctrl-prefix       Configure left controller prefix (required)" >&2
-            echo "  -r or --right-ctrl-prefix      Configure right controller prefix (required)" >&2
-            echo "  -P                             Configure value of \$(P) macro" >&2
-            echo "  -R                             Configure value of \$(R) macro" >&2
-            echo "  -g or --egu                    Configure the engineering units displayed" >&2
-            exit 2
-    esac
+set -e 
 
-    shift 2
+usage () {
+    echo "Usage:" >&2
+    echo "  $1 -t PROCSERV_TELNET_PORT [-P P_VAL] [-R R_VAL] -s SERIAL_NUMBER " >&2
+    echo >&2
+    echo " Options:" >&2
+    echo "  -t                  Configure procServ telnet port" >&2
+    echo "  -P                  Configure value of \$(P) macro" >&2
+    echo "  -R                  Configure value of \$(R) macro" >&2
+    echo "  -l                  Configure left controller prefix (required)" >&2
+    echo "  -r                  Configure right controller prefix (required)" >&2
+    echo "  -g                  Configure the engineering units displayed" >&2
+}
+
+while getopts ":t:P:R:l:r:g:" opt; do
+  case $opt in
+    t) DEVICE_TELNET_PORT="$OPTARG" ;;
+    P) P="$OPTARG" ;;
+    R) R="$OPTARG" ;;
+    l) CTRL_LEFT="$OPTARG" ;;
+    r) CTRL_RIGHT="$OPTARG" ;;
+    g) EGU="$OPTARG" ;; 
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage $0
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage $0
+      exit 1
+      ;;
+  esac
 done
 
+# if getopts did not process all input
+if [ "$OPTIND" -le "$#" ]; then
+    echo "Invalid argument at index '$OPTIND' does not have a corresponding option." >&2
+    usage $0
+    exit 1
+fi
--- iocBoot/iocDiffCtrl/runProcServ.sh
+++ iocBoot/iocDiffCtrl/runProcServ.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+set -e
+set +u
+
+# Parse command-line options
+. ./parseCMDOpts.sh "$@"
+
 # Use defaults if not set
-if [ -z "${DIFF_DEVICE_TELNET_PORT}" ]; then
-   DIFF_DEVICE_TELNET_PORT=20000
+if [ -z "${DEVICE_TELNET_PORT}" ]; then
+   DEVICE_TELNET_PORT="20000"
 fi
 
+if [ -z "${DIFF_INSTANCE}" ]; then
+   DIFF_INSTANCE="1"
+fi
+
+set -u
+
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n diffCtrl${DIFF_INSTANCE} -i ^C^D ${DIFF_DEVICE_TELNET_PORT} ./runDiffCtrl.sh "$@"
+/usr/local/bin/procServ -f -n diffCtrl_${DIFF_INSTANCE} -i ^C^D ${DEVICE_TELNET_PORT} ./runDiffCtrl.sh "$@"
