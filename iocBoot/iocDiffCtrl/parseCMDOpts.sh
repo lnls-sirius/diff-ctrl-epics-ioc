8b302b0f30ff0da1b3ca5d3102b9d9930e09df63
root <root@TA-TiRack-CO-FWSrv-1.abtlus.org.br>
2021-07-12 15:49:45 -0300

    Log & Debug

--- iocBoot/iocDiffCtrl/parseCMDOpts.sh
+++ iocBoot/iocDiffCtrl/parseCMDOpts.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-set -e 
+set -x
+set -e
 
 usage () {
     echo "Usage:" >&2
--- iocBoot/iocDiffCtrl/runProcServ.sh
+++ iocBoot/iocDiffCtrl/runProcServ.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-set -e
+set -xe
 set +u
 
 # Parse command-line options
@@ -19,7 +18,7 @@ set -u
 
 # Run run*.sh scripts with procServ
 if [ "${UNIX_SOCKET}" ]; then
-    /usr/local/bin/procServ -f -n diffCtrl_${DIFF_INSTANCE} -i ^C^D unix:./procserv.sock ./runDiffCtrl.sh "$@"
+    /usr/local/bin/procServ --logfile - -f -n diffCtrl_${DIFF_INSTANCE} -i ^C^D unix:./procserv.sock ./runDiffCtrl.sh "$@"
 else
     /usr/local/bin/procServ -f -n diffCtrl_${DIFF_INSTANCE} -i ^C^D ${DEVICE_TELNET_PORT} ./runDiffCtrl.sh "$@"
 fi
